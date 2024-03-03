3982b35490c37a2890fd861f578dd16192ca792d
akilvishnum <akilvishnum@gmail.com>
2024-03-03 22:38:48 +0530

    Scroll to bottom on initial load

--- src/ResponseContainer.jsx
+++ src/ResponseContainer.jsx
@@ -19,23 +19,25 @@ export const ResponseContainer = () => {
     const stickyRef = useRef(null);
     const [comment, setComment] = useState('');
     // const [stickyHeight, setStickyHeight] = useState(0);
-    // useEffect(() => {
-    //     setStickyHeight(stickyRef.current.clientHeight + 15);
-    // }, [comment])
+    useEffect(() => {
+        stickyRef.current.scrollIntoView({behavior: "smooth"})
+    })
 
     return (
         <Box sx = {{width: '60%', background: 'transparent', borderRadius: '20px'}}>
             <Box sx = {{position: 'sticky', top: 0, background: 'white', width: '100%', boxSizing: 'border-box', borderRadius: '20px 20px 0px 0px', border: '1px solid #c2c2c2de'}}>
                     <ResponseContainerHeader orderId={123} productName = {'GitHub'} />
             </Box>
-            <Box sx = {{width: '100%', background: '#f3f3f3', height: '70vh', overflow: 'scroll', boxSizing: 'border-box', borderLeft: '1px solid #c2c2c2de', borderRight: '1px solid #c2c2c2de'}}>
+            <Box sx = {{width: '100%', background: '#f3f3f3', height: '70vh', overflow: 'auto', boxSizing: 'border-box', borderLeft: '1px solid #c2c2c2de', borderRight: '1px solid #c2c2c2de'}}>
                 <Box sx = {{width: '100%', padding: '1rem', display: 'flex', flexDirection: 'column', gap: '2rem', boxSizing: 'border-box'}}>
                     <AwaitingComment commentedBy={'Akilvishnu.M@wellsfargo.com'} time = {'11.53PM'}/>
                     <RespondComment />
                     <AwaitingComment commentedBy={'Akilvishnu.M@wellsfargo.com'} time = {'11.53PM'}/>
                     <RespondComment />
                 </Box>
+                <div ref = {stickyRef} />
             </Box>
+            
             <Box sx = {{position: 'sticky', width: '100%', bottom: 0, display: 'flex', zIndex: 100,  padding: '0.77rem 0.5rem', boxSizing: 'border-box', border: '1px solid #c2c2c2de', background: 'white', borderRadius: '0 0 20px 20px'}}>
                 <Grid container sx = {{display: 'flex', alignItems: 'flex-end'}} spacing = {1}>
                     <Grid item xs = {10}>
